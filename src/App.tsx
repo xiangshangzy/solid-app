b009211a7cc0185b63bedd0d52602ccea9b50bcf
up <13410704085@163.com>
2024-10-27 00:55:45 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
-import Consul from 'consul'
-const consul=new Consul()
+import { ConfigConsul } from './lib/consul';
+ConfigConsul()
 const App = () => {
   return (
     <div class="content">
       <h1>Rsbuild with Solid</h1>
       <p>Start building amazing things with Rsbuild.</p>
+      <h1 class="px-2 text-blue items-center justify-between">Hello world!</h1>
     </div>
   );
 };
